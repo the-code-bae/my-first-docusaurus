9f138f81c3da6150c0a2ff2ab9ac8c08216dc421
Maka I <nibe0993@gmail.com>
2024-04-28 11:28:05 +0100

    remove deployment branch param

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,6 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'the-code-bae', // Usually your GitHub org/user name.
   projectName: 'my-first-docusaurus', // Usually your repo name.
-  deploymentBranch: 'main', // Deployment branch
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
