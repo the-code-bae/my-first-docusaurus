b5057e611556c04d3b6cd14e899645865dc2be5b
Maka I <nibe0993@gmail.com>
2024-04-28 11:04:14 +0100

    update url param

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com/the-code-bae/',
+  url: 'https://the-code-bae.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/my-first-docusaurus/',
